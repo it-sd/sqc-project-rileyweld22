6c9b80b09fb4ef6915bf3dc16d945d7697dce8c4
Riley Weld <rweld1@student.cvtc.edu>
2023-02-20 13:08:32 -0600

    Making and using a database

---
+++ docs/schema.sql
@@ -0,0 +1,28 @@
+\encoding UTF8
+
+DROP TABLE IF EXISTS chart;
+DROP TABLE IF EXISTS tune_chart;
+
+CREATE TABLE chart (
+    id SERIAL PRIMARY KEY,
+    number INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    time TIMESTAMP WITH TIME ZONE NOT NULL,
+    creator TEXT NOT NULL
+);
+
+CREATE TABLE tune_chart (
+    id SERIAL PRIMARY KEY,
+    chart_id INTEGER NOT NULL,
+    number INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    artist TEXT NOT NULL,
+    album TEXT NOT NULL,
+    length INTEGER NOT NULL,
+    cover TEXT NULL
+);
+
+INSERT INTO tune_chart (chart_id, number, name, artist, album, length)
+    values
+ (1, 1, 'Woods', 'Mac Miller', 'Circles', 286),
+ (1, 2, 'THE zone~', 'Lil Yachty', 'Let''s Start Here.', 249);
---
+++ spec/support/serverSpec.js
@@ -0,0 +1,70 @@
+const { queryAllSongs } =
+  require('../../server.js')
+
+describe('song server', function () {
+    const baseUrl = 'http://localhost:5163'
+    const shouldBeAbove200 = async function (route) {
+        it('should be above 200', async function () {
+        const url = new URL(route, baseUrl)
+        const res = await fetch(url)
+        expect(res.status).toBeGreaterThanOrEqual(200)
+        }, 10000)
+    }
+    const shouldBeLessThan399 = async function (route) {
+        it('should be below 399', async function () {
+        const url = new URL(route, baseUrl)
+        const res = await fetch(url)
+        expect(res.status).toBeLessThanOrEqual(399)
+        }, 10000)
+    }
+    describe("GET '/health'", function () {
+        shouldBeAbove200('/healthy')
+    })
+    describe("GET '/'", function () {
+        shouldBeAbove200('/')
+    })
+    describe("GET '/health'", function () {
+        shouldBeLessThan399('/health')
+    })
+    describe("GET '/'", function () {
+        shouldBeLessThan399('/')
+    })
+
+
+    describe('queryAllSongs', function () {
+        it('should return more than 2 songs', async function () {
+            const results = await queryAllSongs()
+            expect(results).toBeDefined()
+            expect(results.songs).toBeDefined()
+            expect(results.songs.length).toBeGreaterThanOrEqual(2)
+        })
+    })
+
+    describe('querySong', function () {
+        beforeEach(async function () {
+        this.results = await queryAllSongs(1)
+        })
+
+        const shouldHave = function (source, property) {
+            expect(source).toBeDefined()
+            expect(source + "." + property).toBeDefined()
+        }
+
+        it('should return a number', function () {
+            shouldHave(this.results, 'number')
+        })
+        it('should return a name', function () {
+            shouldHave(this.results, 'name')
+        })
+        it('should return a artist', function () {
+            shouldHave(this.results, 'artist')
+        })
+        it('should return a album', function () {
+            shouldHave(this.results, 'album')
+        })
+        it('should return a length', function () {
+            shouldHave(this.results, 'length')
+        })
+    })
+
+})
